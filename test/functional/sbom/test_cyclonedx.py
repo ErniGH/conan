f5232a45eb64923e1769a9b7a8a1b481ad737b01
Ernesto de Gracia Herranz <ernestodgh@jfrog.com>
2025-01-23 12:59:16 +0100

    fix tests

--- test/functional/sbom/test_cyclonedx.py
+++ test/functional/sbom/test_cyclonedx.py
@@ -46,7 +46,7 @@ def test_sbom_generation_create(hook_setup_post_package_tl):
     # bar -> engine/1.0 -> matrix/1.0
     tc.run("create . --build=missing")
     bar_layout = tc.created_layout()
-    assert os.path.exists(os.path.join(bar_layout.metadata(), "cyclonedx_1_4.json"))
+    assert os.path.exists(os.path.join(bar_layout.metadata(), "sbom.cdx.json"))
 
 def test_sbom_generation_skipped_dependencies(hook_setup_post_package):
     tc = hook_setup_post_package
@@ -60,7 +60,7 @@ def test_sbom_generation_skipped_dependencies(hook_setup_post_package):
     tc.run("create .")
     create_layout = tc.created_layout()
 
-    cyclone_path = os.path.join(create_layout.metadata(), "cyclonedx_1_4.json")
+    cyclone_path = os.path.join(create_layout.metadata(), "sbom.cdx.json")
     content = tc.load(cyclone_path)
     # A skipped dependency also shows up in the sbom
     assert "pkg:conan/dep@1.0?rref=6a99f55e933fb6feeb96df134c33af44" in content
@@ -76,11 +76,12 @@ from conan.tools.sbom.cyclonedx import cyclonedx_1_4
 
 def post_generate(conanfile):
     sbom_cyclonedx_1_4 = cyclonedx_1_4(conanfile.subgraph)
-    metadata_folder = conanfile.package_metadata_folder
+    generators_folder = conanfile.generators_folder
     file_name = "sbom.cdx.json"
-    with open(os.path.join(metadata_folder, file_name), 'w') as f:
+    os.mkdir(os.path.join(generators_folder, "sbom"))
+    with open(os.path.join(generators_folder, "sbom", file_name), 'w') as f:
         json.dump(sbom_cyclonedx_1_4, f, indent=4)
-    ConanOutput().success(f"CYCLONEDX CREATED - {conanfile.package_metadata_folder}")
+    ConanOutput().success(f"CYCLONEDX CREATED - {conanfile.generators_folder}")
 """
 
 @pytest.fixture()
@@ -99,7 +100,7 @@ def test_sbom_generation_install_requires(hook_setup_post_generate):
 
     #cli -> foo -> dep
     tc.run("install --requires=foo/1.0")
-    assert os.path.exists(os.path.join(tc.current_folder, "cyclonedx_1_4.json"))
+    assert os.path.exists(os.path.join(tc.current_folder, "sbom", "sbom.cdx.json"))
 
 def test_sbom_generation_install_path(hook_setup_post_generate):
     tc = hook_setup_post_generate
@@ -109,7 +110,7 @@ def test_sbom_generation_install_path(hook_setup_post_generate):
 
     #foo -> dep
     tc.run("install .")
-    assert os.path.exists(os.path.join(tc.current_folder, "cyclonedx_1_4.json"))
+    assert os.path.exists(os.path.join(tc.current_folder, "sbom", "sbom.cdx.json"))
 
 def test_sbom_generation_install_path_consumer(hook_setup_post_generate):
     tc = hook_setup_post_generate
@@ -119,7 +120,7 @@ def test_sbom_generation_install_path_consumer(hook_setup_post_generate):
 
     #conanfile.py -> dep
     tc.run("install .")
-    assert os.path.exists(os.path.join(tc.current_folder, "cyclonedx_1_4.json"))
+    assert os.path.exists(os.path.join(tc.current_folder, "sbom", "sbom.cdx.json"))
 
 def test_sbom_generation_install_path_txt(hook_setup_post_generate):
     tc = hook_setup_post_generate
@@ -134,4 +135,4 @@ def test_sbom_generation_install_path_txt(hook_setup_post_generate):
 
     #foo -> dep
     tc.run("install .")
-    assert os.path.exists(os.path.join(tc.current_folder, "cyclonedx_1_4.json"))
+    assert os.path.exists(os.path.join(tc.current_folder, "sbom", "sbom.cdx.json"))
